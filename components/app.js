bcd4fd3983150eb8ef15b62f451516e0739b6d9f
Jordan Schaefer <jschaef99@gmail.com>
2020-03-20 13:21:09 -0700

    Get Grades returning the required info

--- components/app.js
+++ components/app.js
@@ -1,5 +1,5 @@
-class App{
-  constructor(){
+class App {
+  constructor() {
     this.handleGetGradeSuccess = this.handleGetGradeSuccess.bind(this);
     this.handleGetGradesError = this.handleGetGradesError.bind(this);
   }
@@ -9,12 +9,17 @@ class App{
   handleGetGradeSuccess(grades){
     console.log(grades);
   }
+
   getGrades(){
     $.ajax({
       method: "GET",
       url: "https://sgt.lfzprototypes.com/api/grades",
+      headers: {"x-access-token": "7eg7zS3x"},
       success: this.handleGetGradeSuccess,
       error: this.handleGetGradesError
     })
+  }
+  start(){
+    this.getGrades();
   }
 }
