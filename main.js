a8d33bd39c719ffa9ecf737a8329eda684015bd2
Jordan Schaefer <jschaef99@gmail.com>
2020-03-20 16:40:04 -0700

    header loading properly

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-var header = document.querySelector("header");
-
-var pageHeader = new PageHeader(header);
-
 var table = document.querySelector("table");
-
 var gradeTable = new GradeTable(table);
+
+var header = document.querySelector("header");
+var pageHeader = new PageHeader(header);
 
 var app = new App(gradeTable, pageHeader);
 app.start();
