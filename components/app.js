637b3de266fe204b51d06f98bb2b5f2c2515e674
Jordan Schaefer <jschaef99@gmail.com>
2020-03-20 13:05:08 -0700

    Get Grades function

--- components/app.js
+++ components/app.js
@@ -2,12 +2,19 @@ class App{
   constructor(){
     this.handleGetGradeSuccess = this.handleGetGradeSuccess.bind(this);
     this.handleGetGradesError = this.handleGetGradesError.bind(this);
-  },
+  }
   handleGetGradesError(error){
     console.error(error);
-  },
+  }
   handleGetGradeSuccess(grades){
     console.log(grades);
-  },
-
+  }
+  getGrades(){
+    $.ajax({
+      method: "GET",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      success: this.handleGetGradeSuccess,
+      error: this.handleGetGradesError
+    })
+  }
 }
