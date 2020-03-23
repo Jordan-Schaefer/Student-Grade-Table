728f90c4c161fd9bab028d514040ca9cf08a203e
Jordan Schaefer <jschaef99@gmail.com>
2020-03-23 09:56:59 -0700

    Instantiating the form

--- components/grade-form.js
+++ components/grade-form.js
@@ -1,11 +1,13 @@
 class GradeForm {
   constructor(formElement){
     this.formElement = formElement;
+    this.formElement = this.handleSubmit.bind(this);
   }
   onsubmit(createGrade){
     this.createGrade = createGrade;
   }
-  handleSubmit(){
-
+  handleSubmit(event){
+    event.preventDefault();
+    console.log("Hi from handle submit");
   }
 }
