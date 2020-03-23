be5a1654fc69ad0f37ee284553b0c64c066cab51
Jordan Schaefer <jschaef99@gmail.com>
2020-03-23 14:56:35 -0700

    changed grade tables update method

--- components/grade-table.js
+++ components/grade-table.js
@@ -1,54 +1,73 @@
 class GradeTable {
-  constructor(tableElement){
+  constructor(tableElement, noGradesElement) {
     this.tableElement = tableElement;
+    this.noGradesElement = noGradesElement;
   }
-  updateGrades (grades){
+  updateGrades(grades) {
 
     console.log(grades);
     var tBody = this.tableElement.querySelector("tbody");
-    while(tBody.firstChild){
+    var p = document.querySelector("p");
+    while (tBody.firstChild) {
       tBody.removeChild(tBody.firstChild);
     }
-    for (var i = 0; i < grades.length; i++){
-      var tr = document.createElement("tr");
-      var name = document.createElement("td");
-      var course = document.createElement("td");
-      var grade = document.createElement("td");
+    for (var i = 0; i < grades.length; i++) {
+      // var tr = document.createElement("tr");
+      // var name = document.createElement("td");
+      // var course = document.createElement("td");
+      // var grade = document.createElement("td");
 
-      name.textContent = grades[i].name;
-      course.textContent = grades[i].course;
-      grade.textContent = grades[i].grade;
+      // name.textContent = grades[i].name;
+      // course.textContent = grades[i].course;
+      // grade.textContent = grades[i].grade;
 
-      tr.appendChild(name);
-      tr.appendChild(course);
-      tr.appendChild(grade);
-      tBody.appendChild(tr);
+      // tr.appendChild(name);
+      // tr.appendChild(course);
+      // tr.appendChild(grade);
+      // tBody.appendChild(tr);
+
+      this.renderGradeRow(grades[i], this.deleteGrade);
+
+    }
+    if(grades){
+      p.classList.remove(".d-none");
+    }else{
+      p.classList.add(".d-none");
     }
   }
 
-  onDeleteClick(deleteGrade){
+  onDeleteClick(deleteGrade) {
     this.deleteGrade = deleteGrade;
   }
 
-  renderGradeRow(data, deleteGrade){
-    var row = document.createElement("tr");
-    var name = document.createElement("td");
-    var course = document.createElement("td");
-    var grade = document.createElement("td");
-    var del = document.createElement("td");
-    var delButton = document.createElement("button");
+  renderGradeRow(data, deleteGrade) {
+    var tbody = document.querySelector("tbody");
 
-    name.textContent = data.name;
-    course.textContent = data.course;
-    grade.textContent = data.grade;
-    delButton.textContent = "DELETE";
+    // for (var i = 0; i < data.length; i++) {
+      var row = document.createElement("tr");
+      var name = document.createElement("td");
+      var course = document.createElement("td");
+      var grade = document.createElement("td");
+      var del = document.createElement("td");
+      var delButton = document.createElement("button");
 
-    del.appendChild(delButton);
-    row.appendChild(name);
-    row.appendChild(course);
-    row.appendChild(grade);
-    row.appendChild(del);
+      name.textContent = data.name;
+      course.textContent = data.course;
+      grade.textContent = data.grade;
+      delButton.textContent = "DELETE";
+      delButton.classList.add("btn btn-danger")
 
+      del.appendChild(delButton);
+      row.appendChild(name);
+      row.appendChild(course);
+      row.appendChild(grade);
+      row.appendChild(del);
+      tbody.appendChild(row);
+
+      delButton.addEventListener("click", this.deleteGrade(data.id));
+    // }
+
+    return row;
 
   }
 }
